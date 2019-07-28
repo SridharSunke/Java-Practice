f406392998e8b6cc539d60a5187acb710f4c2747
sankarnathb <sankarnath4u@gmail.com>
2019-07-28 20:44:35 +0530

    Commit

--- Java-Sample/src/com/samples/thread/ArrayBlockingQueueSample.java
+++ Java-Sample/src/com/samples/thread/ArrayBlockingQueueSample.java
@@ -1,5 +1,17 @@
 package com.samples.thread;
 
-public class ArrayBlockingQueueSample {
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
+public class ArrayBlockingQueueSample {
+public static void main(String[] args) {
+	BlockingQueue<Message> queue = new ArrayBlockingQueue<>(10);
+    Producer producer = new Producer(queue);
+    Consumer consumer = new Consumer(queue);
+    //starting producer to produce messages in queue
+    new Thread(producer).start();
+    //starting consumer to consume messages from queue
+    new Thread(consumer).start();
+    System.out.println("Producer and Consumer has been started");
+}
 }
--- Java-Sample/src/com/samples/thread/VolatileExample.java
+++ Java-Sample/src/com/samples/thread/VolatileExample.java
@@ -2,4 +2,5 @@ package com.samples.thread;
 
 public class VolatileExample {
 
+	
 }
