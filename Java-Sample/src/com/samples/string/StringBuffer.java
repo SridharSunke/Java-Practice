4ffba872df9b39aa36cd52a9649a4ab2578313fe
sankarnathb <sankarnath4u@gmail.com>
2019-07-29 12:09:35 +0530

    cmmit

---
+++ Java-Sample/src/com/samples/string/StringBuffer.java
@@ -0,0 +1,5 @@
+package com.samples.string;
+
+public class StringBuffer {
+
+}
--- Java-Sample/src/com/samples/thread/DemonThreadSample.java
+++ Java-Sample/src/com/samples/thread/DemonThreadSample.java
@@ -1,5 +1,23 @@
 package com.samples.thread;
 
-public class DemonThreadSample {
+public class DemonThreadSample implements Runnable{
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		DemonThreadSample t1= new DemonThreadSample();
+		Thread t2= new Thread(t1);
+		t2.setDaemon(true);
+		t2.start();
+	}
 
 }
