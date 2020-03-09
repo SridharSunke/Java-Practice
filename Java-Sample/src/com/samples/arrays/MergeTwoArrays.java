6f2ecb8f173ca8daf1d35547e00a286f98dea630
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:34:47 +0530

    Update MergeTwoArrays.java

--- Java-Sample/src/com/samples/arrays/MergeTwoArrays.java
+++ Java-Sample/src/com/samples/arrays/MergeTwoArrays.java
@@ -1,5 +1,22 @@
 package com.samples.arrays;
 
-public class MergeTwoArrays {
+import java.util.Arrays;
+import java.util.stream.Stream;
 
-}
+public class MergeTwoArrays {
+	
+	static String[] str= {"Sankar","Mouni","Hemanth"};
+	static String[] str2= {"Mouni","Sankar","Hemanth"};
+	
+
+	public static void main(String[] args) {
+		
+		  int aLen = str.length;
+	        int bLen = str2.length;
+	        int[] result = new int[aLen + bLen];
+	        System.arraycopy(str, 0, result, 0, aLen);
+	        System.arraycopy(str2, 0, result, aLen, bLen);
+	}
+	}
+
+
