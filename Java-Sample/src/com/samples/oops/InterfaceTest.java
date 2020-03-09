6e2849e280aa6b99ff60c3bb42a4c4d3a44d17f5
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:51:09 +0530

    Update InterfaceTest.java

--- Java-Sample/src/com/samples/oops/InterfaceTest.java
+++ Java-Sample/src/com/samples/oops/InterfaceTest.java
@@ -1,1 +1,22 @@
 
+package com.samples.oops;
+
+public class interfaceTest implements interfacesample{
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	//we can increase from default to public
+	@Override
+	public void sub() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
