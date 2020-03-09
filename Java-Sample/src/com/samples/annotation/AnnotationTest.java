a226c30cd7d0e25ef43eb5f1054d05051bd4d50b
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:32:25 +0530

    Update AnnotationTest.java

--- Java-Sample/src/com/samples/annotation/AnnotationTest.java
+++ Java-Sample/src/com/samples/annotation/AnnotationTest.java
@@ -1,5 +1,15 @@
 package com.samples.annotation;
 
+import com.samples.exceptionhandling.JsonSerializationException;
+
 public class AnnotationTest {
 
+	public static void main(String[] args) throws JsonSerializationException {
+	Student s= new Student();
+	System.out.print(s.clone());
+	s.setId("1");
+	s.setName("sankar");
+	JsonSerializer serialize= new JsonSerializer();
+	System.out.print(serialize.serialize(s));
+	}
 }
