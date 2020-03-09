df345d71a4b0fcdc68f35fb6001d385be344500a
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 18:06:08 +0530

    Update StringPoolQuetions.java

--- Java-Sample/src/com/samples/string/StringPoolQuetions.java
+++ Java-Sample/src/com/samples/string/StringPoolQuetions.java
@@ -1,5 +1,28 @@
 package com.samples.string;
 
 public class StringPoolQuetions {
+	public static void main(String[] args) {
+	
+		String s="Sankar";
+		 String s2=s;
+		 
+		 s="Sridhar";
+		 
+		 
+		 String s3=s;
+		 s=s.replace("a", "e");
+		String n= new String("XYZ");
+		//n="ss";
+		n=new String("www");
+		n=n+"ss";
+		System.out.println(s2);
+		System.out.println(s);
+		System.out.println(s3);
 
-}
+		System.out.println(n);
+		
+		
+
+	}
+	}
+
