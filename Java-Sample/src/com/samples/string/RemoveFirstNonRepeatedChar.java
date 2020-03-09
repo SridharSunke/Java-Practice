16bddbe1d76c783dd46df976b7ef41e4b1535da6
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 18:04:33 +0530

    Update RemoveFirstNonRepeatedChar.java

--- Java-Sample/src/com/samples/string/RemoveFirstNonRepeatedChar.java
+++ Java-Sample/src/com/samples/string/RemoveFirstNonRepeatedChar.java
@@ -1,5 +1,35 @@
 package com.samples.string;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 public class RemoveFirstNonRepeatedChar {
 
+	
+	public static void main(String[] args) {
+		String s1="abcda";
+
+		findNonRepeatedChar(s1);
+		
+		
+	}
+
+	private static void findNonRepeatedChar(String s1) {
+		HashMap<String,Integer> map = new HashMap<>();
+		for(String c: s1.split("")) {
+			 map.put(c,map.containsKey(c) ? map.get(c)+1:1);			
+		}
+		for(Entry<String, Integer> entry :map.entrySet()) {
+			if(entry.getValue()==1) {
+				System.out.print(entry.getKey());
+				break;
+			}
+			
+		}
+		
+	}
+	
+
+		
+		
 }
