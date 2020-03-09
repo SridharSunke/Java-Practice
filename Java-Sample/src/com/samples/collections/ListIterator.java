df689236e3e95787af7f3a618091d11766d7bffa
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:41:47 +0530

    Update ListIterator.java

--- Java-Sample/src/com/samples/collections/ListIterator.java
+++ Java-Sample/src/com/samples/collections/ListIterator.java
@@ -1,5 +1,20 @@
 package com.samples.collections;
 
-public class ListIterator {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.ListIterator;
+import java.util.Map;
 
+public class ListIteratorSample {
+	public static void main(String args[]){  
+
+	ArrayList<String> al=new ArrayList<String>();
+	ListIterator<String> str= al.listIterator();
+		str.hasNext();
+		
+		LinkedHashMap<Integer,String> map2=new LinkedHashMap<Integer,String>();
+		Iterator<Map.Entry<Integer,String>> lit= map2.entrySet().iterator();
+		str.hasNext();
+	}
 }
