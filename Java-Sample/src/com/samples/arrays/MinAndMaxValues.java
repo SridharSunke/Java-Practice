c9a13d5fc2131c385b5528f31b2661e12c7f4c47
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:35:18 +0530

    Update MinAndMaxValues.java

--- Java-Sample/src/com/samples/arrays/MinAndMaxValues.java
+++ Java-Sample/src/com/samples/arrays/MinAndMaxValues.java
@@ -2,4 +2,42 @@ package com.samples.arrays;
 
 public class MinAndMaxValues {
 
+	
+public static void main(String[] args) {
+		
+		
+		int[] arr= {1,23,43,2,33,44,55,44,22,29,3,54};
+		System.out.print(minValue(arr));
+		System.out.print(maxValue(arr));
+}
+
+private static int maxValue(int[] arr) {
+	int i; 
+    
+    // Initialize maximum element 
+    int max = arr[0]; 
+   
+    // Traverse array elements from second and 
+    // compare every element with current max   
+    for (i = 1; i < arr.length; i++) 
+        if (arr[i] > max) 
+            max = arr[i]; 
+   
+    return max; 	
+}
+
+private static int minValue(int[] arr) {
+	  // Initialize maximum element 
+	
+	int i;
+    int min = arr[0]; 
+   
+    // Traverse array elements from second and 
+    // compare every element with current max   
+    for (i = 1; i < arr.length; i++) 
+        if (arr[i] < min) 
+            min = arr[i]; 
+   
+    return min; 	
+}
 }
