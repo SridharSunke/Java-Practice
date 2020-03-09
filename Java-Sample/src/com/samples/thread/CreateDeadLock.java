6400998e3a060323d4a9e48df901cd13b4c8be86
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:57:09 +0530

    Update CreateDeadLock.java

--- Java-Sample/src/com/samples/thread/CreateDeadLock.java
+++ Java-Sample/src/com/samples/thread/CreateDeadLock.java
@@ -1,1 +1,53 @@
 
+package com.samples.thread;
+
+public class CreateDeadLock {
+	
+	static String s="ss";
+	static String s2="zz";
+	
+	public static void main(String[] args) {
+		
+		Thread t1=new Thread(new MyRunnableDeadLock());
+		Thread t2=new Thread(new MyRunnableDeadLock2());
+t1.start();
+t2.start();
+		
+	}
+	public static class MyRunnableDeadLock implements Runnable{
+
+		@Override
+		public void run() {
+			synchronized (s){
+				
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				synchronized (s2){}
+				
+			}
+			
+		}}
+	
+	public static class MyRunnableDeadLock2 implements Runnable{
+
+		@Override
+		public void run() {
+			synchronized (s2){
+				
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				synchronized (s){}
+				
+			}
+			
+		}}
+
+}
