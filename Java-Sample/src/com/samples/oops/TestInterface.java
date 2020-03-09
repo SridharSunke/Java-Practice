14e457e15813505b0224d19e0e5588176d9183d4
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:52:22 +0530

    Update TestInterface.java

--- Java-Sample/src/com/samples/oops/TestInterface.java
+++ Java-Sample/src/com/samples/oops/TestInterface.java
@@ -1,1 +1,17 @@
 
+package com.samples.oops;
+
+public class TestInterface {
+	
+	public static void main(String[] args) {
+		
+		interfaceTest it= new interfaceTest();
+		it.testDefault();
+		interfacesample.testStatic();
+		
+		AbstarctTest ab= new AbstarctTest();
+		ab.ss();
+		
+	}
+
+}
