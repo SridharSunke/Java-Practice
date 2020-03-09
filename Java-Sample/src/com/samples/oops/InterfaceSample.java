e4c07ac7d7ce294e4c4d4bcecdeb192b29ba5c58
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:50:50 +0530

    Update InterfaceSample.java

--- Java-Sample/src/com/samples/oops/InterfaceSample.java
+++ Java-Sample/src/com/samples/oops/InterfaceSample.java
@@ -1,1 +1,17 @@
 
+package com.samples.oops;
+
+public interface interfacesample {
+
+	//interface cannot have constructors
+	//public interfacesample() {}
+	//we can create abstract methods wothout abstract keyword
+	public  void add();
+	abstract void sub();
+default void testDefault() {
+	System.out.print("default");
+}
+	
+	static void testStatic() {System.out.print("Static");
+	}
+}
