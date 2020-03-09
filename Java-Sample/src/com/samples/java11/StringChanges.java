d1eec4998e085b26876a925c416e73d69ed4ee38
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:47:45 +0530

    Update StringChanges.java

--- Java-Sample/src/com/samples/java11/StringChanges.java
+++ Java-Sample/src/com/samples/java11/StringChanges.java
@@ -3,3 +3,27 @@ package com.samples.java11;
 public class StringChanges {
 
 }
+package com.samples.java11;
+
+
+
+public class StringChanges {
+
+	public static void main(String[] args) {
+
+//	var multiline = "This\r\nis a\r\nmultiline\r\nstring";
+//
+//	multiline.lines()
+//
+//	    // we now have a `Stream<String>`
+//
+//	    .map(line -> "// " + line)
+//
+//	    .forEach(System.out::println);
+		
+//System.out.println(" ".isBlank()); //true
+		
+		//System.out.println(" ".isBlank()); //true
+
+	}
+}
