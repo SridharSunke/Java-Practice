fb38ecc3ed6893d876d55c34f0ac4447371a71c7
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:50:24 +0530

    Update AbstractTest.java

--- Java-Sample/src/com/samples/oops/AbstractTest.java
+++ Java-Sample/src/com/samples/oops/AbstractTest.java
@@ -1,1 +1,21 @@
 
+package com.samples.oops;
+
+public class AbstarctTest extends AbstractSample{
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	//we can increase the visibility
+	@Override
+	public int div(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
