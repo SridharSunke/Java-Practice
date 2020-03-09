4c23463099882de799ae5f33b1e95105829fcde1
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:53:14 +0530

    Update ReflectionSample.java

--- Java-Sample/src/com/samples/reflection/ReflectionSample.java
+++ Java-Sample/src/com/samples/reflection/ReflectionSample.java
@@ -1,6 +1,8 @@
 package com.samples.reflection;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import com.samples.oops.MethodOverLoading;
@@ -8,15 +10,84 @@ import com.samples.oops.MethodOverLoading;
 public class ReflectionSample {
 
 	
-	public static void main(String[] args) {
-		Method[] methods =MethodOverLoading.class.getMethods();
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchFieldException {
+		Method[] methods2 =MethodOverLoading.class.getMethods();
 
-		for(Method method : methods){
+		for(Method method : methods2){
 			method.setAccessible(true);
 		    System.out.println("method = " + method.getName());
 		}
 		
 		Field[] fields   = MethodOverLoading.class.getFields();
+		
+		
+		
+		
+	    // Creating object whose property is to be checked 
+        Test obj = new Test(); 
+  
+        // Creating class object from the object using 
+        // getclass method 
+        Class cls = obj.getClass(); 
+        System.out.println("The name of class is " + 
+                            cls.getName()); 
+  
+        // Getting the constructor of the class through the 
+        // object of the class 
+        Constructor constructor = cls.getConstructor(); 
+        System.out.println("The name of constructor is " + 
+                            constructor.getName()); 
+  
+        System.out.println("The public methods of class are : "); 
+  
+        // Getting methods of the class through the object 
+        // of the class by using getMethods 
+        Method[] methods = cls.getMethods(); 
+  
+        // Printing method names 
+        for (Method method:methods) 
+            System.out.println(method.getName()); 
+  
+        // creates object of desired method by providing the 
+        // method name and parameter class as arguments to 
+        // the getDeclaredMethod 
+        Method methodcall1 = cls.getDeclaredMethod("method2", 
+                                                 int.class); 
+  
+        // invokes the method at runtime 
+        methodcall1.invoke(obj, 19); 
+  
+        // creates object of the desired field by providing 
+        // the name of field as argument to the  
+        // getDeclaredField method 
+        Field field = cls.getDeclaredField("s"); 
+  
+        // allows the object to access the field irrespective 
+        // of the access specifier used with the field 
+        field.setAccessible(true); 
+  
+        // takes object and the new value to be assigned 
+        // to the field as arguments 
+        field.set(obj, "JAVA"); 
+  
+        // Creates object of desired method by providing the 
+        // method name as argument to the getDeclaredMethod 
+        Method methodcall2 = cls.getDeclaredMethod("method1"); 
+  
+        // invokes the method at runtime 
+        methodcall2.invoke(obj); 
+  
+        // Creates object of the desired method by providing 
+        // the name of method as argument to the  
+        // getDeclaredMethod method 
+        Method methodcall3 = cls.getDeclaredMethod("method3"); 
+  
+        // allows the object to access the method irrespective  
+        // of the access specifier used with the method 
+        methodcall3.setAccessible(true); 
+  
+        // invokes the method at runtime 
+        methodcall3.invoke(obj); 
 	
 	}
 }
