c072ca412278cfa83b47b0f68845abf9b66e8559
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 18:05:42 +0530

    Update StringBuider.java

--- Java-Sample/src/com/samples/string/StringBuider.java
+++ Java-Sample/src/com/samples/string/StringBuider.java
@@ -1,5 +1,45 @@
 package com.samples.string;
 
-public class StringBuider {
+public class StringBuiderSample {
 
+	public static void main(String[] args) {
+
+	// create a StringBuilder object 
+    // usind StringBuilder() constructor 
+    StringBuilder str 
+        = new StringBuilder(); 
+
+    str.append("GFG"); 
+
+    // print string 
+    System.out.println("String = "
+                       + str.toString()); 
+
+    // create a StringBuilder object 
+    // usind StringBuilder(CharSequence) constructor 
+    StringBuilder str1 
+        = new StringBuilder("AAAABBBCCCC"); 
+
+    // print string 
+    System.out.println("String1 = "
+                       + str1.toString()); 
+
+    // create a StringBuilder object 
+    // usind StringBuilder(capacity) constructor 
+    StringBuilder str2 
+        = new StringBuilder(10); 
+
+    // print string 
+    System.out.println("String2 capacity = "
+                       + str2.capacity()); 
+
+    // create a StringBuilder object 
+    // usind StringBuilder(String) constructor 
+    StringBuilder str3 
+        = new StringBuilder(str1); 
+
+    // print string 
+    System.out.println("String3 = "
+                       + str3.toString());
+	}
 }
