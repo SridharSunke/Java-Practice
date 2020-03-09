0ee03915ea2377fb3d3f2ff7d1d691a823dc9e34
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:49:01 +0530

    Update SingletonSample.java

--- Java-Sample/src/com/samples/miscellaneous/SingletonSample.java
+++ Java-Sample/src/com/samples/miscellaneous/SingletonSample.java
@@ -1,1 +1,33 @@
 
+package com.samples.miscellaneous;
+
+import java.io.Serializable;
+
+public class SingletonSample implements Serializable {
+
+    private static volatile SingletonSample sSoleInstance;
+
+    //private constructor.
+    private SingletonSample(){
+
+        //Prevent form the reflection api.
+        if (sSoleInstance != null){
+            throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
+        }
+    }
+
+    public static SingletonSample getInstance() {
+        if (sSoleInstance == null) { //if there is no instance available... create new one
+            synchronized (SingletonSample.class) {
+                if (sSoleInstance == null) sSoleInstance = new SingletonSample();
+            }
+        }
+
+        return sSoleInstance;
+    }
+
+    //Make singleton from serialize and deserialize operation.
+    protected SingletonSample readResolve() {
+        return getInstance();
+    }
+}
