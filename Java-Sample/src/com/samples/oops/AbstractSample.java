af51fc8b2ebf59f52c9a178c40057efcf420d6ee
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:49:52 +0530

    Update AbstractSample.java

--- Java-Sample/src/com/samples/oops/AbstractSample.java
+++ Java-Sample/src/com/samples/oops/AbstractSample.java
@@ -1,1 +1,38 @@
 
+package com.samples.oops;
+
+public abstract class AbstractSample {
+	
+	int a=0;
+	
+	{
+		
+		a=20;
+	}
+	
+	public abstract int add(int a,int b);
+	
+	 abstract int div(int a,int b);
+	 
+	 //cannot create private
+	//private abstract int sub(int a,int b);
+	 
+	 
+//only abstract methods should be implemented by default in reference class
+	 private int mul(int a,int b) {
+		 return a*b;
+	 }
+	
+	 //constrctor possible in abstarct class
+	 public AbstractSample() {}
+	 
+	 
+	 //should be abstract if no body
+	// public int plus(int a, int b);
+	 
+	 
+	 static void ss()
+	   {
+	      System.out.println("Human isting");
+	   }
+}
