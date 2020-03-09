2addd88355a321747be82c21c80cf1fa08248e2a
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 17:39:46 +0530

    Update ComparatorSampl2.java

--- Java-Sample/src/com/samples/collections/ComparatorSampl2.java
+++ Java-Sample/src/com/samples/collections/ComparatorSampl2.java
@@ -1,5 +1,47 @@
 package com.samples.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
 public class ComparatorSampl2 {
 
+	public static void main(String[] args) {
+		String s1="abcda";
+
+		highestOccurance(s1);
+		
+		
+	}
+
+	private static void highestOccurance(String s1) {
+		HashMap<String,Integer> map = new HashMap<>();
+		for(String c: s1.split("")) {
+			 map.put(c,map.containsKey(c) ? map.get(c)+1:1);			
+		}
+
+        List<Entry<String, Integer>> listOfEntries = new ArrayList<Entry<String, Integer>>(map.entrySet());
+
+        
+        //collections.reverseorder to reverse a comparator
+		Collections.sort(listOfEntries,Collections.reverseOrder(new Comparator<Entry<String, Integer>>(){
+
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				Integer v1 = o1.getValue();
+                Integer v2 = o2.getValue();
+                return v1.compareTo(v2);			
+			}}));
+		
+		for(Entry<String, Integer> entry :listOfEntries) {
+			if(entry.getValue() != null) {
+				System.out.print(entry.getKey());
+				break;
+			}
+			
+		}
+	}
 }
