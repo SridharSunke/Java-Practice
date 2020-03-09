f03fb45a0abcf6f525de020a7a5b543406c45f89
sankarnathb <49778521+sankarnathb@users.noreply.github.com>
2020-03-09 18:06:33 +0530

    Update StringReverse.java

--- Java-Sample/src/com/samples/string/StringReverse.java
+++ Java-Sample/src/com/samples/string/StringReverse.java
@@ -1,5 +1,30 @@
 package com.samples.string;
 
-public class StringReverse {
 
+//use recuursion
+public class StringReverse {
+	
+	public static void main(String[] args) {
+
+		String s="Sankar";
+		String s2="";
+
+		System.out.print(reversedString(s));
+
+		
+	}
+
+	private static String reversedString(String s) {
+		if(s.isEmpty()) {
+			System.out.print(s);	
+			return s;
+			
+		}
+		return reversedString(s.substring(1))+s.charAt(0);
+		 
+	}
+	
+
+	
+	
 }
